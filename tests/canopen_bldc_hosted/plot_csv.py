5a5a01fadca36a214a2502b013b37a856190665d
Michael Jossen <michael.jossen@rwth-aachen.de>
2024-08-24 11:29:35 +0200

    Formatting

--- tests/canopen_bldc_hosted/plot_csv.py
+++ tests/canopen_bldc_hosted/plot_csv.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import matplotlib
-matplotlib.use('GTK4Agg')
+
+matplotlib.use("GTK4Agg")
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('sim_motor.csv')
+df = pd.read_csv("sim_motor.csv")
 
 plt.rcParams["figure.autolayout"] = True
-fig, (ax1,ax2,ax3,ax4) = plt.subplots(4, 1)
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
 # ax1.plot(df.Time, df.v1, label="v1")
 # ax1.plot(df.Time, df.v2, label="v2")
 # ax1.plot(df.Time, df.v3, label="v3")
@@ -17,24 +18,24 @@ ax1.plot(df.Time, df.pwm, label="pwm")
 ax1.plot(df.Time, df.g1, label="g1")
 ax1.plot(df.Time, df.g2, label="g2")
 ax1.plot(df.Time, df.g3, label="g3")
-ax1.set_xlabel('Time/s')
+ax1.set_xlabel("Time/s")
 ax1.legend()
 
 ax2.plot(df.Time, df.i1, label="i1")
 ax2.plot(df.Time, df.i2, label="i2")
 ax2.plot(df.Time, df.i3, label="i3")
-ax2.set_xlabel('Time/s')
+ax2.set_xlabel("Time/s")
 ax2.legend()
 
 ax3.plot(df.Time, df.theta, label="theta")
 ax3.plot(df.Time, df.omega, label="omega")
-ax3.set_xlabel('Time/s')
+ax3.set_xlabel("Time/s")
 ax3.legend()
 
 ax4.plot(df.Time, df.te, label="t_e")
 ax4.plot(df.Time, df.tl, label="t_l")
 ax4.plot(df.Time, df.tf, label="t_f")
-ax4.set_xlabel('Time/s')
+ax4.set_xlabel("Time/s")
 ax4.legend()
 
 plt.show()
--- tests/canopen_bldc_hosted/plot_test_csv.py
+++ tests/canopen_bldc_hosted/plot_test_csv.py
@@ -1,16 +1,16 @@
-
 from functools import partial
 import pandas as pd
 import matplotlib
+
 matplotlib.use("GTK4Agg")
-import signal 
+import signal
 import sys
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-def read_csvs(i,axs):
+def read_csvs(i, axs):
     df_4 = pd.read_csv("sim_motor_4.csv")
     df_6 = pd.read_csv("sim_motor_6.csv")
     df_8 = pd.read_csv("sim_motor_8.csv")
@@ -18,28 +18,29 @@ def read_csvs(i,axs):
     df_12 = pd.read_csv("sim_motor_12.csv")
     df_16 = pd.read_csv("sim_motor_16.csv")
     df_18 = pd.read_csv("sim_motor_18.csv")
-    datas = [df_4,df_6,df_8,df_10,df_12,df_16,df_18]
-    names = ["4","6","8","10","12","16","18"]
-    for k,(ax,data,name) in enumerate(zip(axs.flat,datas,names)):
+    datas = [df_4, df_6, df_8, df_10, df_12, df_16, df_18]
+    names = ["4", "6", "8", "10", "12", "16", "18"]
+    for k, (ax, data, name) in enumerate(zip(axs.flat, datas, names)):
         try:
-            ax.get_lines()[0].set_data(data.Time,data.omega)
+            ax.get_lines()[0].set_data(data.Time, data.omega)
         except:
-            ax.plot(data.Time,data.omega)
-        ax.set_xlabel('Time', fontsize=12)
-        ax.set_ylabel('Omega', fontsize=12)
+            ax.plot(data.Time, data.omega)
+        ax.set_xlabel("Time", fontsize=12)
+        ax.set_ylabel("Omega", fontsize=12)
         ax.set_title("Motor {}".format(name), fontsize=14)
         factor = 1.2
         seconds = 20
-        x_max = data.Time.max()+seconds*200
-        x_min = x_max - seconds*1000
-        y_lim = max(10,abs(data.omega.max()),abs(data.omega.min()))*factor
-        ax.set_xlim(x_min,x_max)
-        ax.set_ylim(-y_lim,y_lim)
+        x_max = data.Time.max() + seconds * 200
+        x_min = x_max - seconds * 1000
+        y_lim = max(10, abs(data.omega.max()), abs(data.omega.min())) * factor
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(-y_lim, y_lim)
 
 
 def on_close(event):
     sys.exit(0)
-    
+
+
 def signal_handler(sig, frame):
     sys.exit(0)
 
@@ -49,9 +50,9 @@ def main():
     plt.ion()
     plt.style.use("fivethirtyeight")
     fig, axs = plt.subplots(nrows=4, ncols=2, constrained_layout=True)
-    fig.canvas.mpl_connect('close_event', on_close)
+    fig.canvas.mpl_connect("close_event", on_close)
     while True:
-        csv_func = partial(read_csvs,axs=axs)
+        csv_func = partial(read_csvs, axs=axs)
         ani = FuncAnimation(fig, csv_func, interval=1000)
 
         plt.legend()
