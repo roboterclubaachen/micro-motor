23732cafe9fe0069db608c3a6d0ac8ac7419fb37
Michael Jossen <michael.jossen@rwth-aachen.de>
2024-11-27 02:12:48 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -17,35 +17,35 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        lbuild-python-package = pkgs.python311Packages.buildPythonPackage rec {
+        lbuild-python-package = pkgs.python3Packages.buildPythonPackage rec {
           pname = "lbuild";
           version = "1.21.8";
-          src = pkgs.python311Packages.fetchPypi {
+          src = pkgs.python3Packages.fetchPypi {
             inherit pname version;
             sha256 = "70abaf36b46a239c1ee4fa9c71e843381b21b9dccede4cd50df392ae78560e57";
           };
           propagatedBuildInputs = [
-            pkgs.python311Packages.lxml
-            pkgs.python311Packages.jinja2
-            pkgs.python311Packages.anytree
-            pkgs.python311Packages.testfixtures
-            pkgs.python311Packages.coverage
-            pkgs.python311Packages.gitpython
-            pkgs.python311Packages.pip
+            pkgs.python3Packages.lxml
+            pkgs.python3Packages.jinja2
+            pkgs.python3Packages.anytree
+            pkgs.python3Packages.testfixtures
+            pkgs.python3Packages.coverage
+            pkgs.python3Packages.gitpython
+            pkgs.python3Packages.pip
           ];
         };
-        modm-python-package = pkgs.python311Packages.buildPythonPackage rec {
+        modm-python-package = pkgs.python3Packages.buildPythonPackage rec {
           pname = "modm";
           version = "0.1.2";
-          src = pkgs.python311Packages.fetchPypi {
+          src = pkgs.python3Packages.fetchPypi {
             inherit pname version;
             sha256 = "4c3edbf7fa945d3fc10c0191d1000ee5b491f3d0e43f363af3b9de12e38a5d12";
           };
           propagatedBuildInputs = [
             lbuild-python-package
-            pkgs.python311Packages.lxml
-            pkgs.python311Packages.pyelftools
-            pkgs.python311Packages.pip
+            pkgs.python3Packages.lxml
+            pkgs.python3Packages.pyelftools
+            pkgs.python3Packages.pip
           ];
         };
       in
@@ -57,7 +57,7 @@
               openocd
               lldb
               gcc-arm-embedded
-              (python311.withPackages (python-pkgs: [
+              (python3.withPackages (python-pkgs: [
                 python-pkgs.pandas
                 python-pkgs.pygobject3
                 python-pkgs.numpy
